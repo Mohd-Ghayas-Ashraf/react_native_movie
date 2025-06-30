aee0b19081ab0431a8d48265e11b6882eb083a03
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-30 11:06:37 +0530

    Install Tailwind,NativeWind,SafeArea

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome to the app.</Text>
+      <Text>Welcome to the app...</Text>
     </View>
   );
 }
