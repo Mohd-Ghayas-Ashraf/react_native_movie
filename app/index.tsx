dd04233fabb240aa402302ea7863d36a10a213e1
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-30 11:47:23 +0530

    Color Customization

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-300">
-      <Text className="text-blue-500 font-bold text-5xl">
+      <Text className="text-dark-200 font-bold text-5xl">
         Welcome to the app with Tailwind
       </Text>
     </View>
