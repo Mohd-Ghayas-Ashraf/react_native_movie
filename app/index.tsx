4d4c9f49b6311bd331c27a556e6976358135f715
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-30 12:02:19 +0530

    Added First Routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,9 @@ export default function Index() {
       <Text className="text-dark-200 font-bold text-5xl">
         Welcome to the app with Tailwind
       </Text>
+      <Link href="/onboarding">
+        <Text>Onboarding</Text>
+      </Link>
     </View>
   );
 }
